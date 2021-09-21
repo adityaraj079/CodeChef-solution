5caad14a671dde08a8cd6e73d5f5f9ad8a55bad5
Aditya Raj <rajaditya079@gmail.com>
2021-09-22 00:14:34 +0530

    ZEBRA done again

--- CookOff/September/ZEBRA.cpp
+++ CookOff/September/ZEBRA.cpp
@@ -3,74 +3,39 @@
 using namespace std;
 #define ll long long int
 
-void solve()
-{
-    ll n,k;
-    cin>>n>>k;
-    string s;
-    cin>>s;
-
-    char res = s[0];
-    int cnt =0;
-    for(int i=1;i<n;i++) {
-        if(res != s[i]) {
-            cnt++;
-            res = s[i];
-        }
-    }
-
-    if(cnt<k) {
-        cout<<-1<<"\n";
-        return;
-    }
-    if(s[0]=='0') {
-        if(k%2) {
-            for(int i=s.size()-1; i>=0; i--) {
-                if(s[i] == '1') {
-                    cout<<i+1<<"\n";
-                    return;
-                }
-            }
-        }
-        else {
-            for(int i=s.size()-1; i>=0; i--) {
-                if(s[i] == '0') {
-                    cout<<i+1<<"\n";
-                    return;
-                }
-            }
-        }
-    }
-    else {
-        if(k%2) {
-            for(int i=s.size()-1; i>=0; i--) {
-                if(s[i] == '0') {
-                    cout<<i+1<<"\n";
-                    return;
-                }
-            }
-        }
-        else {
-            for(int i=s.size()-1; i>=0; i--) {
-                if(s[i] == '1') {
-                    cout<<i+1<<"\n";
-                    return;
-                }
-            }
-        }
-    }
-
-}
 int main()
 {
-    //fast input and output
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
- 
-    int T;
-    cin>>T;
-    while(T--) {
-        solve();
+    ll T; cin>>T;
+    while(T--) 
+    {
+        ll n,k;
+        cin>>n>>k;
+        string zeb;
+        cin>>zeb;
+        ll ans=0, alt=0;
+        for (int i=1; i<n; i++)
+        {
+            if(zeb[i] != zeb[i-1])
+                alt++;
+        }
+        if (alt >= k)
+        {
+            for(int i=n-1; i>=0; i--)
+            {
+                if (k%2 != 0 && zeb[i]!= zeb[0])
+                {
+                    cout<<i+1<<endl;
+                    break;
+                }
+                else if (k%2 == 0 && zeb[i]== zeb[0])
+                {
+                    cout<<i+1<<endl;
+                    break;
+                }
+            }
+        }
+        else
+            cout<<-1<<endl;
     }
     return 0;
 }
