d5132a2f452cb9d14cf926c95453c5dc66a71250
Aditya Raj <rajaditya079@gmail.com>
2021-09-27 23:31:02 +0530

    Prefix sums redone

--- Lunchtime/September/UNQEQ.cpp
+++ Lunchtime/September/UNQEQ.cpp
@@ -1,73 +1,39 @@
-
 #include <bits/stdc++.h> 
 using namespace std; 
 #define ll long long 
-void solve(); 
+
 int main() 
 { 
-    ios_base::sync_with_stdio(false); 
-    cin.tie(NULL); 
-   
-#ifndef ONLINE_JUDGE 
-    freopen("input.txt", "r", stdin); 
-    freopen("error.txt", "w", stderr); 
-    freopen("output.txt", "w", stdout); 
-#endif 
-   
-    ll int t=1; 
-    /*is Single Test case?*/ 
-     
-    cin >> t; 
-     
-    // t=1;  
-    //@cpsolutions 
-    //single test case  -= Delete if t testCases =- 
-     
-    while (t--) { 
-        solve(); 
-        cout << "\n"; 
-    } 
-   
-    cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl; 
-    int z; 
-    cin>>z; 
-    return 0; 
-} 
- 
-void solve() 
-{ 
-    int n;cin>>n; 
-    if(n%4 != 0){ 
-        cout<<"NO"; 
-        return; 
-    } 
-    cout<<"YES"<<endl; 
-    vector<int> v1; 
-    vector<int> v2; 
-    int a = 1; 
-    int b = n; 
-    int flag = 0; 
-    int check = n/2; 
-    while(check){ 
-        if(flag == 0){ 
-            v1.push_back(a); 
-            a++; 
-            flag = 1; 
-        } 
-        else { 
-            v1.push_back(b); 
-            b--; 
-            flag = 0; 
-        } 
-        check--; 
-    } 
-    for(int i=a;i<=b;i++){ 
-        v2.push_back(i); 
-    } 
-    sort(v1.begin(),v1.end()); 
-    sort(v2.begin(),v2.end()); 
-    for(auto i:v1) cout<<i<<" "; 
-    cout<<endl; 
-    for(auto i:v2) cout<<i<<" "; 
+    int t; cin>>t;
+
+    while(t--)
+    {
+        int n; cin>>n;
+        int temp = (n*(n+1))/2;
+        if ( n %4 != 0)
+        {
+            cout<<"NO"<<endl;
+        }
+        else
+        {
+            cout<<"YES"<<endl;
+            int q1=n/4, q2 = 2*n/4, q3 = 3*n/4, q4=n;
+            for (int i=1;i<=q1; i++)
+            {
+                cout<<i<<" ";
+            }
+            for (int i=q3+1; i<=q4; i++)
+            {
+                cout<<i<<" ";
+            }cout<<endl;
+
+            for (int i=q1+1;i<=q3; i++)
+            {
+                cout<<i<<" ";
+            }cout<<endl;
+            
+
+        }
+    }
  
 } 
